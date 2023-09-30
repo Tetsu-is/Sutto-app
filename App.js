4b48f79cc5b117c30cf9c84bc699a7dc7721d1d6
Tatsuru Hisanaga <tatsu72x@gmail.com>
2023-09-30 18:02:08 +0900

    feat: ✨ add basic UI for alarm list

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, FlatList, Text } from 'react-native';
 
 export default function App() {
+  const [alarms, setAlarms] = useState([]);
   return (
     <View style={styles.container}>
       <Text>やったぜー！</Text>
       <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <FlatList
+        data={alarms}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item }) => (
+          <Card containerStyle={{ marginTop: 40 }}>
+            <Card.Title>アラーム設定</Card.Title>
+            <Card.Divider />
+            <Text style={{ marginBottom: 10 }}>
+              時刻: {item.time.toString()}
+            </Text>
+            <Text style={{ marginBottom: 10 }}>
+              曜日: {JSON.stringify(item.days)}
+            </Text>
+          </Card>
+        )}
+      />
     </View>
   );
 }
